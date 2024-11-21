279d567d6c88524195f74ca549fa2ce620f550b1
AbelX18 <abelx8cam@gmail.com>
2024-11-21 05:17:38 -0300

    first commit

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+export function formatCurrency(amount: number){
+    return new Intl.NumberFormat('en-US',{
+        style: 'currency',
+        currency: 'USD'
+    }).format(amount)
+}
+
+export function getImagePath(imagePath: string) {
+    const clodinaryBaseUrl= 'https://res.cloudinary.com'
+    if(imagePath.startsWith(clodinaryBaseUrl)){
+        return imagePath
+    }else{
+        return  `/products/${imagePath}.jpg`
+    }
+
+}
